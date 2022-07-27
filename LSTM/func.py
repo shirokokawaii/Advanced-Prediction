5c3882f3d80bfada57716378f560eaa96d8134a0
ytyonly <daguguguji18@gmail.com>
2022-07-28 07:33:14 +0800

    'divide2three'

--- LSTM/func.py
+++ LSTM/func.py
@@ -14,7 +14,6 @@ def get_data(interval, size, end):
                 '1h': 'https://min-api.cryptocompare.com/data/v2/histohour',
                 '1m': 'https://min-api.cryptocompare.com/data/v2/histominute'}
     Ts = time.mktime(time.strptime(end, "%Y-%m-%d %H:%M:%S"))
-    print(Ts)
     res = requests.get(endpoint[interval] + '?fsym=BTC&tsym=USD&limit=' + str(size) + "&toTs=" + str(Ts))
     if interval != '1d':
         data = json.loads(res.content)['Data']['Data']
@@ -27,20 +26,34 @@ def get_data(interval, size, end):
     return hist
 
 
-def train_test_split(df, test_size=0.2):
-    split_row = len(df) - int(test_size * len(df))
-    train_data = df.iloc[:split_row]
-    test_data = df.iloc[split_row:]
-    return train_data, test_data
+def data_split(df, valid_size=0.2, test_size=0.1):
+    train_row = len(df) - int((valid_size + test_size) * len(df))
+    valid_row = len(df) - int(test_size * len(df))
+    train_data = df.iloc[:train_row]
+    valid_data = df.iloc[train_row:valid_row]
+    test_data = df.iloc[valid_row:]
+    return train_data, valid_data, test_data
 
 
-def line_plot(line1, line2, label1=None, label2=None, title='', lw=2):
+def line_plot(line1, line2, line3, label1=None, label2=None, label3=None, title='', lw=2):
     fig, ax = plt.subplots(1, figsize=(13, 7))
     ax.plot(line1, label=label1, linewidth=lw)
     ax.plot(line2, label=label2, linewidth=lw)
+    ax.plot(line3, label=label3, linewidth=lw)
     ax.set_ylabel('price [USD]', fontsize=14)
     ax.set_title(title, fontsize=16)
-    ax.legend(loc='best', fontsize=16);
+    ax.legend(loc='best', fontsize=16)
+
+
+def line_plot4(line1, line2, line3, line4, label3=None, label4=None, title='', lw=2):
+    fig, ax = plt.subplots(1, figsize=(13, 7))
+    ax.plot(line1, label='actual', linewidth=lw, color='green')
+    ax.plot(line2, linewidth=lw, color='green')
+    ax.plot(line3, label=label3, linewidth=lw)
+    ax.plot(line4, label=label4, linewidth=lw)
+    ax.set_ylabel('price [USD]', fontsize=14)
+    ax.set_title(title, fontsize=16)
+    ax.legend(loc='best', fontsize=16)
 
 
 def normalise_zero_base(df):
@@ -61,17 +74,18 @@ def extract_window_data(df, window_len=5, zero_base=True):
     return np.array(window_data)
 
 
-def prepare_data(df, target_col, window_len=10, zero_base=True, test_size=0.2):
-    train_data, test_data = train_test_split(df, test_size=test_size)
+def prepare_data(df, target_col, window_len=10, zero_base=True, valid_size=0.2, test_size=0.1):
+    train_data, valid_data, test_data = data_split(df, valid_size, test_size)
     X_train = extract_window_data(train_data, window_len, zero_base)
-    X_test = extract_window_data(test_data, window_len, zero_base)
+    X_valid = extract_window_data(valid_data, window_len, zero_base)
     y_train = train_data[target_col][window_len:].values
-    y_test = test_data[target_col][window_len:].values
+    y_valid = valid_data[target_col][window_len:].values
+
     if zero_base:
         y_train = y_train / train_data[target_col][:-window_len].values - 1
-        y_test = y_test / test_data[target_col][:-window_len].values - 1
+        y_valid = y_valid / valid_data[target_col][:-window_len].values - 1
 
-    return train_data, test_data, X_train, X_test, y_train, y_test
+    return train_data, valid_data, test_data, X_train, X_valid, y_train, y_valid
 
 
 def build_lstm_model(input_data, output_size, neurons=100, activ_func='linear',
--- LSTM/predict.py
+++ LSTM/predict.py
@@ -5,39 +5,44 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
-from LSTM.func import train_test_split, line_plot, build_lstm_model, prepare_data, get_data
+from LSTM.func import data_split, line_plot, build_lstm_model, prepare_data, get_data, line_plot4
 
-end = "2021-05-05 20:00:00"
-time_intervial = '1h'
+end = "2022-07-05 20:00:00"
+time_interval = '1d'
 data_size = 500
-window_len = 5
+valid_size = 0.1
 test_size = 0.1
 
-hist = get_data(time_intervial, data_size, end)
+
+hist = get_data(time_interval, data_size, end)
 
 hist.to_csv("data.csv")
 print(hist.head(5))
 print(hist.size)
 target_col = 'close'
-train, test = train_test_split(hist, test_size)
-line_plot(train[target_col], test[target_col], 'training', 'test', title='')
+train, valid, test = data_split(hist, valid_size, test_size)
+line_plot(train[target_col], valid[target_col], test[target_col], 'training', 'valid', 'test', title='')
 plt.show()
 
 np.random.seed(42)
+window_len = 5
 zero_base = True
 lstm_neurons = 100
-epochs = 20
+epochs = 30
 batch_size = 32
 loss = 'mse'
 dropout = 0.2
 optimizer = 'adam'
-train, test, X_train, X_test, y_train, y_test = prepare_data(
-    hist, target_col, window_len=window_len, zero_base=zero_base, test_size=test_size)
+
+train, valid, test, X_train, X_valid, y_train, y_valid = prepare_data(
+    hist, target_col, window_len=window_len, zero_base=zero_base, valid_size=valid_size, test_size=test_size)
+
 model = build_lstm_model(
     X_train, output_size=1, neurons=lstm_neurons, dropout=dropout, loss=loss,
     optimizer=optimizer)
+
 history = model.fit(
-    X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)
+    X_train, y_train, validation_data=(X_valid, y_valid), epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)
 
 plt.plot(history.history['loss'], 'r', linewidth=2, label='Train loss')
 plt.plot(history.history['val_loss'], 'g', linewidth=2, label='Validation loss')
@@ -46,14 +51,21 @@ plt.xlabel('Epochs')
 plt.ylabel('MSE')
 plt.show()
 
-targets = test[target_col][window_len:]
-preds = model.predict(X_test).squeeze()
-mean_absolute_error(preds, y_test)
+targets1 = valid[target_col][window_len:]
+targets2 = test[target_col][window_len:]
 
-MAE = mean_squared_error(preds, y_test)
-R2 = r2_score(y_test, preds)
+preds = model.predict(X_valid).squeeze()
+tests = model.predict(X_valid).squeeze()
 
-preds = test[target_col].values[:-window_len] * (preds + 1)
-preds = pd.Series(index=targets.index, data=preds)
-line_plot(targets, preds, 'actual', 'prediction', lw=3)
+mean_absolute_error(preds, y_valid)
+MAE = mean_squared_error(preds, y_valid)
+R2 = r2_score(y_valid, preds)
+
+preds = valid[target_col].values[:-window_len] * (preds + 1)
+preds = pd.Series(index=targets1.index, data=preds)
+
+tests = test[target_col].values[:-window_len] * (tests + 1)
+tests = pd.Series(index=targets2.index, data=tests)
+
+line_plot4(targets1, targets2, preds, tests, 'fit', 'predict', lw=2)
 plt.show()
