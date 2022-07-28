aa23bce4ce9701a16877e3bbd37a986f478b52ec
ytyonly <daguguguji18@gmail.com>
2022-07-28 22:32:27 +0800

    'update_data'

--- utils/test.py
+++ utils/test.py
@@ -6,10 +6,10 @@ from utils.get_data import get_data
 DIR="../data/"
 
 
-end = "2015-07-05 20:00:00"
+end = "2011-08-14 20:00:00"
 time_interval = '1d'
-data_size = 10
-filename = "2015-7_"+str(data_size)+"_"+time_interval
+data_size = 2000
+filename = "2017-2_"+str(data_size)+"_"+time_interval
 
 hist = get_data(time_interval, data_size, end, filename)
 
