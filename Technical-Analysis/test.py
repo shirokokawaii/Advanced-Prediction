92ead41bc5e2f0792e8582df97dd09c3f146c71a
as312d <as312d@gmail.com>
2022-07-28 20:07:22 +0800

    add data files

---
+++ Technical-Analysis/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+# predict_K = 10000
+# actual_K = -1
+# if((predict_K * actual_K) > 0):
+#     print('ture')
+# else:
+#     print('false')
+data = np.load('2010-2022,720.npy')
+print(data.shape)
+print(data)
